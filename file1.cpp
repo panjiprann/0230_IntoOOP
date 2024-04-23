0db2e666ad6ef0be2d2ad1a63e241aa975163477
panjiprann <panjipranantya20@gmail.com>
2024-04-23 15:25:05 +0700

    membuat void input

--- file1.cpp
+++ file1.cpp
@@ -28,4 +28,10 @@ class Matakuliah{
             cout << "SKS: ";
             cin >> sks;
         }
+
+        void output(){
+            cout << "Kode MK: " << kodeMk << endl;
+            cout << "Nama MK: " << namaMk << endl;
+            cout << "SKS: " << sks << endl;
+        }
 };
