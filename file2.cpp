d26f20319d065a2f1746f87c5bfd3a3bc05c9907
panjiprann <panjipranantya20@gmail.com>
2024-04-23 15:35:41 +0700

    membuat void display

--- file2.cpp
+++ file2.cpp
@@ -21,6 +21,11 @@ class bangunDatar{
             return panjang * lebar;
         }
 
+        void display (){
+            cout << "Panjangnya : " << panjang << endl;
+            cout << "Lebarnya : " << lebar << endl;
+            cout << "Luasnya  = " << hitungLuas() << endl;
+        }
 };
 
 
